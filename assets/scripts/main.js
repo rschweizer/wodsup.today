2b00e4737ed10bbea8cf672b159e62a1b0a80f80
Robert Schweizer <mail@rschweizer.me>
2019-03-30 14:53:55 +0100

    Remove CTA and add announcement

--- assets/scripts/main.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('section.cta input#box')
-input.addEventListener(
-  'keyup',
-  event =>
-    event.keyCode === 13 &&
-    input.value !== '' &&
-    (window.location = '/' + input.value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase())
-)
